dbe4175b0c5e2306793461bfd5edda377705cc6f
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-02-03 00:19:55 +0500

    Selenium is addded to project for infinite scroll

--- AltNews/spiders/altnews.py
+++ AltNews/spiders/altnews.py
@@ -1,10 +1,33 @@
-import scrapy
+from scrapy import Spider
+from scrapy.http import Request 
+from scrapy.selector import Selector
 
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-class AltnewsSpider(scrapy.Spider):
+from time import sleep
+
+chrome_driver_path = r"D:\Projects\Assignments\DataScience\Web Scrapers\chromedriver.exe"
+
+class AltnewsSpider(Spider):
     name = "altnews"
     allowed_domains = ["altnews.in"]
-    start_urls = ["https://altnews.in"]
+    # start_urls = ["https://altnews.in"]
+    
+    def start_requests(self):
+        self.service = Service(chrome_driver_path) 
+        self.driver = webdriver.Chrome(service=self.service) 
+        self.driver.maximize_window()
+        self.driver.get("https://altnews.in")
+        sleep(5)
+        
+        
+        
+        
+        
+        sel = Selector(text=self.driver.page_source())
 
     def parse(self, response):
         pass
