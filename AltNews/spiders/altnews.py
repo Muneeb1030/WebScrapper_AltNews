615090cfa3b2d1eaa3f6b36de4f761724fa9ff8e
Muhammad Muneeb ur Rehman <m.muneeb.ur.rehman.2000@gmail.com>
2024-02-03 12:04:11 +0500

    First Page News List is Scrapped

--- AltNews/spiders/altnews.py
+++ AltNews/spiders/altnews.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from scrapy import Spider
 from scrapy.http import Request 
 from scrapy.selector import Selector
@@ -12,13 +13,18 @@ import os
 
 chrome_driver_path = r"D:\Projects\Assignments\DataScience\Web Scrapers\chromedriver.exe"
 
+
 class AltnewsSpider(Spider):
     name = "altnews"
     allowed_domains = ["altnews.in"]
     # start_urls = ["https://altnews.in"]
+    Tags = []
+    DateValue = []
+    Headlines = []
+    Urls =[]
+    Author = []
     
-    def start_requests(self):
-        
+    def start_requests(self) -> Iterable[Request]:
         self.CreateDirectories()
         
         self.service = Service(chrome_driver_path) 
@@ -27,11 +33,10 @@ class AltnewsSpider(Spider):
         self.driver.get("https://altnews.in")
         sleep(5)
         
-        content = self.driver.find_elements(By.CLASS_NAME,'pbs-content')[3]
-        content.find_elements(By.CLASS_NAME,'post')
+        self.Scrolling()
+        
+        return super().start_requests()
             
-        
-        
     def CreateDirectories(self):
         output_directory = 'output_data'
         if not os.path.exists(output_directory):
@@ -44,8 +49,26 @@ class AltnewsSpider(Spider):
         images_sub_directory = f'{output_directory}/news_articles_Images'
         if not os.path.exists(images_sub_directory):
             os.makedirs(images_sub_directory) 
+    
+    def Scrolling(self):
+        content = self.driver.find_elements(By.XPATH,'//*[@class="pbs-content"]')[3]
+        for Article in content.find_elements(By.TAG_NAME,'article'):
+            receivingData = self.parseNews(Article)
+    
+    
+    def parseNews(Article):
+        try:
+            tag = Article.find_element(By.CLASS_NAME,'cat-tag').text
+            Date = Article.find_element(By.TAG_NAME,'time').text
+            Headline =  Article.find_element(By.TAG_NAME,'h4').text
+            PostUrl = Article.find_element(By.XPATH,'.//h4/a').get_attribute("href")
+            Author = Article.find_element(By.XPATH,'//span[@class="author vcard"]//a[@class="url fn n"]').text
+            returningData = [tag,Date,Headline,PostUrl,Author]
+        except:
+            returningData = ['tag','Date','Headline','PostUrl','Author']
+            
+        return returningData
         
-           
-
     def parse(self, response):
         pass
+        
